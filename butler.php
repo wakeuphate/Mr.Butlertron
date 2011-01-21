a8b368f7bf425d82eccc5561ac1b650276254e14
Wakeuphate <markleetch@gmail.com>
2011-01-21 21:26:53 +0000

    Adding the regex to check for youtube links

--- butler.php
+++ butler.php
@@ -2,24 +2,32 @@
 include_once('lib/SmartIRC.php');
 class butlertron
 {
-    function test(&$irc, &$data)
+  
+  function youtube(&$irc, &$data)
+  {
+    if(preg_match('/http:\/\/www\.youtube[^"]+/', $data->message, $matches) != 0)
     {
-        $irc->message(SMARTIRC_TYPE_CHANNEL, '#butlertron', $data->nick.': It works!');
+      $irc->message(SMARTIRC_TYPE_CHANNEL, '#butlertron', 'Mate, You just posted this youtube link: '.$matches[0]); 
     }
-    function quit(&$irc, &$data)
+  }
+  
+  
+  //todo: authentication for turning mr butlertron off, not based on username.
+  function quit(&$irc, &$data)
+  {
+    if ($data->nick == "Wakeuphate")
     {
-            if ($data->nick == "Wakeuphate")
-            {
-                $irc->disconnect();
-            }
+      $irc->disconnect();
     }
+  }
+  
 }
 $bot = &new butlertron();
 $irc = &new Net_SmartIRC();
 $irc->setDebug(SMARTIRC_DEBUG_ALL);
 $irc->setUseSockets(TRUE);
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!test', $bot, 'test');
-$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '^!quit', $bot, 'quit');
+$irc->registerActionhandler(SMARTIRC_TYPE_CHANNEL, '/http:\/\/www\.youtube[^"]+/', $bot, 'youtube');
+$irc->registerActionhandler(SMARTIRC_TYPE_QUERY|SMARTIRC_TYPE_NOTICE, '^!quit', $bot, 'quit');
 $irc->connect('irc.synirc.net', 6667);
 $irc->login('MrButlertron', 'Mr. Butlertron', 0, 'MrButlertron');
 $irc->join('#butlertron');
